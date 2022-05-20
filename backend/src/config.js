51f16b5b0d0352155bc4d9c5ae923e8deb5397e4
EagleTest <ediane.dbc@gmail.com>
2022-05-21 03:55:44 +0800

    test

--- backend/src/config.js
+++ backend/src/config.js
@@ -9,7 +9,7 @@ const network = NETWORK.eth;
 // General metadata for Ethereum
 const namePrefix = "Eagle Arena";
 const description = "Eagle arena is a game filled with fantastic battle ready Eagles, where players can collect them as pets";
-const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
+const baseUri = "ipfs://bafybeigjebsb5ktuf5wta7lhjfj5s46db75gq5uraem6lkq2izn2rov2ry/"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
@@ -73,7 +73,7 @@ const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
-const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
+const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
 const GENERIC_DESCRIPTION = "Which Eagle you will get?"; // Replace with what you want the generic descriptions to say.
 const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafkreigw5opphluok4ufyfaf5z62euoq5qrcizh56kv3xnpblgzqcla5da"; // Replace with your generic image that will display for all NFTs pre-reveal.
