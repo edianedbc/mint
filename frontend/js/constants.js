13308cdb2fe9b12cc4dde252c44cd639fe6dbf3d
1123123ggg <dummydn08@gmail.com>
2022-05-27 21:27:01 +0800

    asd

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xeFBB5C7Ab1708c7b5601f60cd2202f7aE441897F"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
