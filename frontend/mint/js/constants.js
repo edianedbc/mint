e00032c5ff15a677c92c3bd9b8787ad5b2b1f533
EagleTest <ediane.dbc@gmail.com>
2022-05-27 11:32:09 +0800

    55sad

--- frontend/mint/js/constants.js
+++ frontend/mint/js/constants.js
@@ -1,5 +1,5 @@
 const contractAddress = "0x70C539dc2d01ECb544AEbD041f7faBA5bC51d59c"; //Replace with your own contract address
-const chain = 'polygon'; // rinkeby or polygon
+const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Eagle Arena NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
