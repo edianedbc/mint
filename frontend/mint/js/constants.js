7f291e3c6f2b5766a84ff208e71bb12f608e1457
1123123ggg <dummydn08@gmail.com>
2022-06-08 09:26:12 +0800

    sss

--- frontend/mint/js/constants.js
+++ frontend/mint/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "0xc43968d639FAdA1f9EA8EfC10B224c87477c7fEa"; //Replace with your own contract address
+const contractAddress = "0xAf1bDFDe60cec6f684FB5da73a7e627Fae740b8C"; //Replace with your own contract address
 const chain = 'polygon';  // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Eagle Arena NFT Project!!";
